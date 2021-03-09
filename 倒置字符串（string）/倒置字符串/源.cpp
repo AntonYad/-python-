277819b8f520357289cff10f0c1f65a9a732d1f5
Anton <651299847@qq.com>
2021-03-09 12:42:51 +0800

    逆置字符串

---
+++ 倒置字符串（string）/倒置字符串/源.cpp
@@ -0,0 +1,2 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>#include <string.h>void reverse(char* start, char* end){    while(start < end)    {char temp = *start;*start = *end;*end = temp;start++;end--;    }}int main(){    char input[100] gets(input);        reverse(input, input + strlen(input) - 1);       char* start = input;    while(*start)    {char *end = start;while(*end != ' ' && *end)end++;reverse(start, end-1);if(*end)start = end + 1;elsestart = end;    }    printf("%s", input);    return 0;}
